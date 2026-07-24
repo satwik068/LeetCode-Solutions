9dc45fc8edaa66786997e546e6631701465791b1
Satwik Agrawal <satwikagrawal68@gmail.com>
2026-07-25 00:07:16 +0530

    Time: 0 ms (100%), Space: 44.6 MB (16%) - LeetHub

---
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<String> buildArray(int[] target, int n) {
+        List<String> ans = new ArrayList<>();
+        int curr = 1;
+
+        for (int num : target) {
+            while (curr < num) {
+                ans.add("Push");
+                ans.add("Pop");
+                curr++;
+            }
+
+            ans.add("Push");
+            curr++;
+        }
+
+        return ans;
+    }
+}
