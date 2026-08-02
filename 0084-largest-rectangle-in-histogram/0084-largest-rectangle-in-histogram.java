df52d347507bda9dec15907b8c6132c10cc5b809
Satwik Agrawal <satwikagrawal68@gmail.com>
2026-08-03 00:47:19 +0530

    Time: 107 ms (28.36%), Space: 77.8 MB (64.96%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,39 +1,5 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        // int n = heights.length;
-        // int[] r = new int[n];
-        // int[] l = new int[n];
-
-        // Stack<Integer> s = new Stack<>();
-
-        // for(int i=n-1; i>=0; i--){
-        //     while(s.size()>0 && heights[i]<=heights[s.peek()]){
-        //         s.pop();
-        //     }
-        //     if(s.empty()) r[i] = n;
-        //     else r[i] = s.peek();
-        //     s.push(i);
-        // }
-
-        // while(s.size()>0) s.pop();
-
-        // for(int i=0; i<n; i++){
-        //     while(s.size()>0 && heights[i]<=heights[s.peek()]){
-        //         s.pop();
-        //     }
-        //     if(s.empty()) l[i] = -1;
-        //     else l[i] = s.peek();
-        //     s.push(i);
-        // }
-
-        // int ans = 0;
-
-        // for(int i=0; i<n; i++){
-        //     int area = heights[i]*(r[i]-l[i]-1);
-        //     ans = Math.max(ans, area);
-        // }
-        // return ans;
-
         int n = heights.length;
         int[] r = new int[n];
         int[] l = new int[n];
@@ -44,27 +10,30 @@ class Solution {
             while(s.size()>0 && heights[i]<=heights[s.peek()]){
                 s.pop();
             }
-            if(s.isEmpty()) r[i] = n;
+            if(s.empty()) r[i] = n;
             else r[i] = s.peek();
             s.push(i);
         }
 
-        while(!s.isEmpty()) s.pop();
+        while(s.size()>0) s.pop();
 
         for(int i=0; i<n; i++){
             while(s.size()>0 && heights[i]<=heights[s.peek()]){
                 s.pop();
             }
-            if(s.isEmpty()) l[i] = -1;
+            if(s.empty()) l[i] = -1;
             else l[i] = s.peek();
             s.push(i);
         }
 
         int ans = 0;
+
         for(int i=0; i<n; i++){
-            int area = heights[i] * (r[i]-l[i]-1);
+            int area = heights[i]*(r[i]-l[i]-1);
             ans = Math.max(ans, area);
         }
         return ans;
+
+        
     }
 }
