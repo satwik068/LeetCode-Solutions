365acf20cb041cdd522dfcd1ebee0d2b74642305
Satwik Agrawal <satwikagrawal68@gmail.com>
2026-08-04 16:36:20 +0530

    Time: 6 ms (39.24%), Space: 47.5 MB (5.33%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.java
+++ 3731-find-missing-elements/3731-find-missing-elements.java
@@ -1,23 +1,40 @@
 class Solution {
     public List<Integer> findMissingElements(int[] nums) {
         int n = nums.length;
-        ArrayList<Integer> list = new ArrayList<>();
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-        for (int x : nums) {
-            min = Math.min(min, x);
-            max = Math.max(max, x);
-            list.add(x);
-        }
+        Arrays.sort(nums);
+
         ArrayList<Integer> ans = new ArrayList<>();
-        if(n==(max-min+1)){
-            return ans;
-        }
-        for(int i=min; i<=max; i++){
-            if(!list.contains(i)){
-                ans.add(i);
+        int x = nums[0];
+        int i = 0;
+        while(i<=n-1){
+            if(nums[i]!=x){
+                ans.add(x);
+                x++;
+            }
+            else{
+                x++;
+                i++;
             }
         }
         return ans;
+
+        // ArrayList<Integer> list = new ArrayList<>();
+        // int min = Integer.MAX_VALUE;
+        // int max = Integer.MIN_VALUE;
+        // for (int x : nums) {
+        //     min = Math.min(min, x);
+        //     max = Math.max(max, x);
+        //     list.add(x);
+        // }
+        // ArrayList<Integer> ans = new ArrayList<>();
+        // if(n==(max-min+1)){
+        //     return ans;
+        // }
+        // for(int i=min; i<=max; i++){
+        //     if(!list.contains(i)){
+        //         ans.add(i);
+        //     }
+        // }
+        // return ans;
     }
 }
