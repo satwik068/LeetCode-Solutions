1cb99178d99ceb6f261fe3d6ee110db17712b0c5
Satwik Agrawal <satwikagrawal68@gmail.com>
2026-08-26 21:17:40 +0530

    Time: 0 ms (100%), Space: 41.8 MB (85.64%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,14 +1,24 @@
 class Solution {
 
-    public int fibo(int n, int[] dp) {
-        if(n<=1) return n;
-        if(dp[n]!=0) return dp[n];
-        int ans = fibo(n-1, dp)+fibo(n-2, dp);
-        dp[n] = ans;
-        return ans;
-    }
+    // public int fibo(int n, int[] dp) {
+    //     if(n<=1) return n;
+    //     if(dp[n]!=0) return dp[n];
+    //     int ans = fibo(n-1, dp)+fibo(n-2, dp);
+    //     dp[n] = ans;
+    //     return ans;
+    // }
     public int fib(int n) {
+        // int[] dp = new int[n+1];
+        // return fibo(n, dp);
+
+        // tabulation dp
+        if(n<=1) return n;
         int[] dp = new int[n+1];
-        return fibo(n, dp);
+        dp[0] = 0; dp[1] = 1;
+        for(int i=2; i<n+1; i++){
+            dp[i] = dp[i-1] +dp[i-2];
+        }
+        return dp[n];
+
     }
 }
