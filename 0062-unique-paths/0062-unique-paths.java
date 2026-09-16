18549ef84baa854fe276a7dc15319bdbe1bd3b6c
Satwik Agrawal <satwikagrawal68@gmail.com>
2026-09-16 22:43:58 +0530

    Time: 0 ms (100%), Space: 42.1 MB (47.55%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,29 +1,18 @@
-// class Solution {
-//     public int count(int m, int n, int[][] dp) {
-//         if(m==1 || n==1) return 1;
-//         if(dp[m-1][n-1] != 0) return dp[m-1][n-1];
-//         int left = count(m-1, n, dp);
-//         int right = count(m, n-1, dp);
-//         return dp[m-1][n-1] = left+right;
-//     }
-//     public int uniquePaths(int m, int n) {
-//         int[][] dp = new int[m][n];
-//         return count(m, n, dp);
-//     }
-// }
 class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
 
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(i==0 || j==0) dp[i][j] = 1;
+                if(i==0 || j==0){
+                    dp[i][j] = 1;
+                }
                 else{
                     dp[i][j] = dp[i-1][j] + dp[i][j-1];
                 }
             }
         }
-        return dp[m-1][n-1];
 
+        return dp[m-1][n-1];
     }
 }
