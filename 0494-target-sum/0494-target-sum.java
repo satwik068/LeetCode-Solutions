60454adbe1edc4e397b8eff0883df578ea89ff38
Satwik Agrawal <satwikagrawal68@gmail.com>
2026-09-18 03:24:23 +0530

    Time: 8 ms (60.26%), Space: 46.9 MB (23.2%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,21 +1,25 @@
 class Solution {
-    public int count(int i, int[] nums, int T, int[][] dp){
+    static int sum;
+    public int count(int total, int i, int[] nums, int T, int[][] dp){
         if(i==nums.length){
-            if(T==0) return 1;
+            if(total==T) return 1;
             else return 0;
         }
-        // if(dp[i][T] != -1) return dp[i][T];
-        int skip = count(i+1, nums, T+nums[i], dp);
-        int pick = count(i+1, nums, T-nums[i], dp);
-        return skip + pick;
+        if(dp[i][total+sum] != -1) return dp[i][total+sum];
+        int skip = count(total+nums[i], i+1, nums, T, dp);
+        int pick = count(total-nums[i], i+1, nums, T, dp);
+        return dp[i][total+sum] = skip + pick;
     }
     public int findTargetSumWays(int[] nums, int target) {
-        int t = (target<0)? (-1*target):target;
-        int[][] dp = new int[nums.length][t+1];
+        sum = 0;
+        for(int i:nums){
+            sum += i;
+        }
+        int[][] dp = new int[nums.length][2*sum+1];
         for(int[] a:dp){
             Arrays.fill(a, -1);
         }
-        int ans = count(0, nums, target, dp);
+        int ans = count(0, 0, nums, target, dp);
         return ans;
     }
 }
